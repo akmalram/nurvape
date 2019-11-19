22c105eb7e348d1bd8633a0b848c73d17fc769c2
Akmal <islamov_akmal@inbox.ru>
2019-11-19 15:04:56 +0500

    asdf

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// Styles
+import './general.scss';
+
+function App() {
+    return (
+        <Router>
+            <div>Hello World</div>
+        </Router>
+    )
+}
+
+export default App;
