a7b09e8788a77fcf6eccde0a277160b930f82ae8
akmal <islamov_akmal@inbox.ru>
2019-11-25 17:23:02 +0500

    ad

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
+import Swiper from 'react-id-swiper';
 
+import 'swiper/css/swiper.min.css';
 import './about-styles.scss';
 
 function About() {
+
+    const params = {
+        autoplay: true,
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    }
+
+
     return (
         <div className="about">
             <div className="container">
                 <h1 className="title-primary">О нас</h1>
                 <p className="info">Продукция от компании <strong>NUR VAPE</strong> выпускается с 2016 года. Для производства нашей продукции мы используем только самые качественные и лучшие ингредиенты со всего мира. Мы имеем свое собственное производство и налаженную логистическую цепочку поставок. Доставка осуществляется во все страны мира, за исключеним стран, где запрещена данная продукция. Продукция от <strong>NUR VAPE</strong> реализуется в 10 странах мира. Вместе с продукцией вы получите полный перечень документов.</p>
+                <div className="slider">
+                    <Swiper {...params}>
+                        <div>
+                            <img src="http://maxstroy.uz/nurimages/cheerockeee-banner.jpg" alt="" />
+                        </div>
+                        <div>
+                            <img src="http://maxstroy.uz/nurimages/creamson-banner.jpg" alt="" />
+                        </div>
+                        <div>
+                            <img src="http://maxstroy.uz/nurimages/juicesus_banner.jpg" alt="" />
+                        </div>
+                    </Swiper>
+                </div>
             </div>
+            
         </div>
     )
 }
